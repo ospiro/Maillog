8d274cc752dbfabfbfc5460bc6f691406623e248
Oliver Spiro <Oliver@Olivers-Air.PK5001Z>
2015-06-27 22:14:15 -0400

    Output now unbuffered

--- logscript.sh
+++ logscript.sh
@@ -10,9 +10,9 @@ exit' SIGINT SIGQUIT SIGHUP SIGTERM
 
 # end of trap
 
-#Mail log is CSV, awk is used to parse the input. The first awk chooses the relevent sections from the CSV,sed and the second awk remove the extraneous entries from them.
+#Mail log is CSV, awk is used to parse the input. The first awk chooses the relevent sections from the CSV,sed and the second awk remove the extraneous entries from them. stdbuf -o0 is used to make sure the outputs flush frequently enough to make realtime display viable.
 
-tail -f /var/log/maillog | grep --line-buffered "Passed"|sed s/{.*}/""/| awk -F"," -v ORS="" '{
+stdbuf -o0 tail -f /var/log/maillog|stdbuf -o0 awk '/Passed/'|stdbuf -o0 sed -u s/{.*}/""/|stdbuf -o0 awk -F"," -v ORS="" '{
 
 #Everything from here on is awk.
 
@@ -30,8 +30,7 @@ for(i=10;i<=NF;++i){	        	#This loop makes sure to send the entire subject l
         print $i ",";
     }  	 	                        #Awk normally would delete the commas, so it is necessary to put them back in.
 }
-
-}' | awk -v ORS="" '{
+}' | stdbuf -o0 awk -v ORS="" '{
 
 
 if($9=="MYNETS"){				#This if statement deals with the occasional case wherein the source IP is preceded by "MYNETS LOCAL"
@@ -50,5 +49,4 @@ if ($9!="MYNETS"){				#This is the other case
     }
     print "\n";
 }
-
 }'
